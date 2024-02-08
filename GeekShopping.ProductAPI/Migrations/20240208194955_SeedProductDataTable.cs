070ceca17090d433cea6e284b9af4b5c27638f32
Edinando Vieira <edvieira93@gmail.com>
2024-02-08 17:54:08 -0300

    ✨ feat: Add Project Product API

---
+++ GeekShopping.ProductAPI/Migrations/20240208194955_SeedProductDataTable.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GeekShopping.ProductAPI.Migrations
+{
+    public partial class SeedProductDataTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "product",
+                columns: new[] { "id", "category_name", "description", "image_url", "name", "price" },
+                values: new object[,]
+                {
+                    { 1L, "Colar", "Colar da Sonserina", "https://nerdstore.com.br/produto/colar-flamula-sonserina/", "Colar Flamula Sonserina", 41.9m },
+                    { 2L, "Colar", "Colar da Lufa-Lufa", "https://nerdstore.com.br/wp-content/uploads/2020/11/colar-flamula-lufa-lufa.jpg.webp", "Colar Flamula Lufa-Lufa", 41.9m },
+                    { 3L, "Colar", "Colar da Grifinoria", "https://nerdstore.com.br/wp-content/uploads/2020/11/colar-flamula-grifinoria.jpg.webp", "Colar Flamula Grifinoria", 41.9m },
+                    { 4L, "Colar", "Colar da Corvinal", "https://nerdstore.com.br/wp-content/uploads/2020/11/colar-flamula-corvinal.jpg.webp", "Colar Flamula Corvinal", 41.9m },
+                    { 5L, "Adesivo", "Adesivo Árvore de Gondor", "https://nerdstore.com.br/wp-content/uploads/2023/07/vitrine-adesivo-arvore-de-gondor-02-nerdstore.jpg.webp", "Adesivo Árvore de Gondor", 9m }
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "product",
+                keyColumn: "id",
+                keyValue: 1L);
+
+            migrationBuilder.DeleteData(
+                table: "product",
+                keyColumn: "id",
+                keyValue: 2L);
+
+            migrationBuilder.DeleteData(
+                table: "product",
+                keyColumn: "id",
+                keyValue: 3L);
+
+            migrationBuilder.DeleteData(
+                table: "product",
+                keyColumn: "id",
+                keyValue: 4L);
+
+            migrationBuilder.DeleteData(
+                table: "product",
+                keyColumn: "id",
+                keyValue: 5L);
+        }
+    }
+}
---
+++ GeekShopping.ProductAPI/Model/Context/MySQLContext.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace GeekShopping.ProductAPI.Model.Context
+{
+    public class MySQLContext : DbContext
+    {
+        public MySQLContext() {}
+        public MySQLContext(DbContextOptions<MySQLContext> options) : base(options) {}
+
+        public DbSet<Product> Products { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Product>().HasData(
+                new Product
+                {
+                    Id = 1,
+                    Name = "Colar Flamula Sonserina",
+                    Price = new decimal(41.90),
+                    Description = "Colar da Sonserina",
+                    ImageURL = "https://nerdstore.com.br/produto/colar-flamula-sonserina/",
+                    CategoryName = "Colar"
+                });
+            modelBuilder.Entity<Product>().HasData(
+                new Product
+                {
+                    Id = 2,
+                    Name = "Colar Flamula Lufa-Lufa",
+                    Price = new decimal(41.90),
+                    Description = "Colar da Lufa-Lufa",
+                    ImageURL = "https://nerdstore.com.br/wp-content/uploads/2020/11/colar-flamula-lufa-lufa.jpg.webp",
+                    CategoryName = "Colar"
+                });
+            modelBuilder.Entity<Product>().HasData(
+                new Product
+                {
+                    Id = 3,
+                    Name = "Colar Flamula Grifinoria",
+                    Price = new decimal(41.90),
+                    Description = "Colar da Grifinoria",
+                    ImageURL = "https://nerdstore.com.br/wp-content/uploads/2020/11/colar-flamula-grifinoria.jpg.webp",
+                    CategoryName = "Colar"
+                });
+            modelBuilder.Entity<Product>().HasData(
+                new Product
+                {
+                    Id = 4,
+                    Name = "Colar Flamula Corvinal",
+                    Price = new decimal(41.90),
+                    Description = "Colar da Corvinal",
+                    ImageURL = "https://nerdstore.com.br/wp-content/uploads/2020/11/colar-flamula-corvinal.jpg.webp",
+                    CategoryName = "Colar"
+                });
+            modelBuilder.Entity<Product>().HasData(
+                new Product
+                {
+                    Id = 5,
+                    Name = "Adesivo Árvore de Gondor",
+                    Price = new decimal(9.00),
+                    Description = "Adesivo Árvore de Gondor",
+                    ImageURL = "https://nerdstore.com.br/wp-content/uploads/2023/07/vitrine-adesivo-arvore-de-gondor-02-nerdstore.jpg.webp",
+                    CategoryName = "Adesivo"
+                });
+        }
+    }
+}

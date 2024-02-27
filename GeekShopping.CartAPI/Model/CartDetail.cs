4eb04ecd049820833b926ee52777217e00afab6a
Edinando Vieira <edvieira93@gmail.com>
2024-02-27 14:07:10 -0300

    ✨ feat: Add Entities to DB

---
+++ GeekShopping.CartAPI/Model/CartDetail.cs
@@ -0,0 +1,22 @@
+﻿using GeekShopping.CartAPI.Model.Base;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace GeekShopping.CartAPI.Model
+{
+    [Table("cart_detail")]
+    public class CartDetail : BaseEntity
+    {
+        public long CartHeaderId { get; set; }
+
+        [ForeignKey("CartHeaderId")]
+        public CartHeader CartHeader { get; set; }
+
+        [ForeignKey("ProductId")]
+        public long ProductId { get; set; }
+
+        public Product Product { get; set; }
+
+        [Column("count")]
+        public int Count { get; set; }
+    }
+}

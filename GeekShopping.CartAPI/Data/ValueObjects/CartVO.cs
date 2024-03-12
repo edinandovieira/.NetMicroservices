522de6354a4da7d75be1212341bf811f933dc243
Edinando Vieira <edvieira93@gmail.com>
2024-03-12 17:33:16 -0300

    🐞 fix: endpoint ApplyCoupon and RemoveCoupon

--- GeekShopping.CartAPI/Data/ValueObjects/CartVO.cs
+++ GeekShopping.CartAPI/Data/ValueObjects/CartVO.cs
@@ -5,6 +5,6 @@ namespace GeekShopping.CartAPI.Data.ValueObjects
     public class CartVO
     {
         public CartHeaderVO CartHeader { get; set; }
-        public IEnumerable<CartDetailVO> CartDetails { get; set; }
+        public IEnumerable<CartDetailVO>? CartDetails { get; set; }
     }
 }
